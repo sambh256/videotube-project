d5bb1a2ccff306a571428efd06577b1d48f1461f
sambh256 <sambhavpowar@gmail.com>
2025-05-13 20:52:04 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -27,4 +27,12 @@ const app = express();
     console.error("ERROR:", error);
     throw error;
   }
-})();
+})()
+.then(()=>{
+  app.listen(process.env.PORT || 8000, ()=>{
+    console.log(`Server is running at port :${process.env.PORT}`)
+  })
+.catch((err)=>{
+  console.log("MONGO db connection failed!!", err);
+  })
+});
